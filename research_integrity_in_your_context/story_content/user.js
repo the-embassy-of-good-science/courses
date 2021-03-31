0dc758487beebdda50d7d0adafc9f5999821bfa5
armsch <81294702+armsch@users.noreply.github.com>
2021-03-31 18:22:08 +0200

    Add files via upload

---
+++ research_integrity_in_your_context/story_content/user.js
@@ -0,0 +1,91 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5gtgFqYS7XV":
+        Script1();
+        break;
+      case "6lq7HCOFwms":
+        Script2();
+        break;
+      case "6IWXxAsd0if":
+        Script3();
+        break;
+      case "603dUKREBCv":
+        Script4();
+        break;
+      case "69f4HKJ1o4s":
+        Script5();
+        break;
+      case "65r0fsiORFn":
+        Script6();
+        break;
+      case "6URgt022RBC":
+        Script7();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
