1142c5e43429e654cf919c57d718408e77cc8189
armsch <81294702+armsch@users.noreply.github.com>
2021-04-13 12:53:03 +0200

    Delete what_is_research_integrity directory

--- what_is_research_integrity/story_content/5YsUfi3o2iP_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.43.22859.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.011%0D%0ATo%20continue,%20please%20click%20on%20%22Move%20on%22.%0D%0A%0D%0A"}')
--- what_is_research_integrity/story_content/6lS2sfYhL7D_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.43.22859.0%0D%0A"}')
