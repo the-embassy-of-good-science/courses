f4cae93cf79422fff4895a918164d5f3c378e1a8
armsch <81294702+armsch@users.noreply.github.com>
2021-04-13 12:53:47 +0200

    Delete three_levels_of_research_integrity directory

--- three_levels_of_research_integrity/story_content/6iZTu1jzUde_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.43.22859.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.613%0D%0AThe%20Hong%20%20Kong%20Principles%20address%20five%20issues:%0D%0A%0D%0A"}')
