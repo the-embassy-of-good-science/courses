5970a661689f4e33e15a04b4b470d35c1eaf9743
armsch <81294702+armsch@users.noreply.github.com>
2021-05-28 16:49:42 +0200

    Add files via upload

---
+++ Responsible_supervision_mentoring_role-modeling/story_content/user.js
@@ -0,0 +1,79 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6V1nJFZxK4a":
+        Script1();
+        break;
+      case "65r667Woi0r":
+        Script2();
+        break;
+      case "6o7UAGxQT3t":
+        Script3();
+        break;
+      case "64cyLByXR0n":
+        Script4();
+        break;
+      case "5gOttwCIdAw":
+        Script5();
+        break;
+      case "6qheHy9wZsq":
+        Script6();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
