e1d9ffab6ffff6a7c9302bdafae23f62ace620d4
armsch <81294702+armsch@users.noreply.github.com>
2021-05-31 12:42:10 +0200

    Add files via upload

---
+++ Roles_and_responsibilities_in_supervision_and_mentoring/story_content/5gP5NKeRmU7_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.50.24668.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:05.930%0D%0AThe%20mentor%20keeps%20the%20best%20interest%20of%20the%20mentee%20in%20mind,%20and%20is%20thus%20acting%20selflessly.%0D%0A%0D%0A"}')
---
+++ Roles_and_responsibilities_in_supervision_and_mentoring/story_content/5quoGEKWhSX_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.50.24668.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:06.609%0D%0AGet%20training%20in%20supervision,%20and/or%20find%20a%20more%20experienced%20supervisor%20to%20mentor%20you%0D%0A%0D%0A"}')
