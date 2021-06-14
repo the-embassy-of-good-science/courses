f2c129f312cb3b56639e619aa2a55888f9ca7763
armsch <81294702+armsch@users.noreply.github.com>
2021-06-14 17:38:00 +0200

    Add files via upload

---
+++ virtue_ethics_and_research_integrity/story_content/user.js
@@ -0,0 +1,127 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6Bpto9rSmlP":
+        Script1();
+        break;
+      case "6HDS1W6ZrpN":
+        Script2();
+        break;
+      case "5WX5pMvFW45":
+        Script3();
+        break;
+      case "6RwG0PY2RTY":
+        Script4();
+        break;
+      case "6g9ycQmrejD":
+        Script5();
+        break;
+      case "6EX5oTkefLN":
+        Script6();
+        break;
+      case "6nzqZlw1WOe":
+        Script7();
+        break;
+      case "5u5bdrpnNPZ":
+        Script8();
+        break;
+      case "6a3ydBmFUtU":
+        Script9();
+        break;
+      case "680nzHqMSwm":
+        Script10();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script10()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
