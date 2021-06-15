f57d681010de6222dffcb8b06e90c6de357390df
armsch <81294702+armsch@users.noreply.github.com>
2021-06-15 15:07:07 +0200

    Delete virtues_in_research directory

--- virtues_in_research/story_content/5fMOxARhJbf_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.43.22859.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:01.704%0D%0AAccording%20to%20James%20Rachels,%0D%0A%0D%0A00:00:01.730%20--%3E%2000:00:02.900%0D%0A%20a%20virtue%20is%20a%0D%0A%0D%0A"}')
--- virtues_in_research/story_content/6H1nvoIxCgd_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.43.22859.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:03.187%0D%0ATo%20begin%20with,%20let\u0027s%20look%20at%20a%20definition%20of%20virtues.%0D%0A%0D%0A"}')
