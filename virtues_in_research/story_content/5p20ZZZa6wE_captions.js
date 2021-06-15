1a25fa613fa7ec2fbf0f4eb1a9a627bc44696511
armsch <81294702+armsch@users.noreply.github.com>
2021-06-15 16:33:44 +0200

    Add files via upload

---
+++ virtues_in_research/story_content/5p20ZZZa6wE_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.43.22859.0%0D%0A%0D%0A00:00:00.078%20--%3E%2000:00:02.717%0D%0A...%20can%20become%20quickly%20outdated%20by%20the%20work%20of%20others.%20%0D%0A%0D%0A"}')
