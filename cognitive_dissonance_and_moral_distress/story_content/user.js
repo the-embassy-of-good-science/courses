f287ffc3e047aede4246d8bccc91818e446feac2
armsch <81294702+armsch@users.noreply.github.com>
2021-06-22 16:41:26 +0200

    Add files via upload

---
+++ cognitive_dissonance_and_moral_distress/story_content/user.js
@@ -0,0 +1,115 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "63ac7mKZp6g":
+        Script1();
+        break;
+      case "6G46yejOGVN":
+        Script2();
+        break;
+      case "6NbKDk1xwoE":
+        Script3();
+        break;
+      case "6hQd47WD4Kw":
+        Script4();
+        break;
+      case "6UKEtDDCjdY":
+        Script5();
+        break;
+      case "6rS82q5Ooc9":
+        Script6();
+        break;
+      case "6fvK02Drz4a":
+        Script7();
+        break;
+      case "6D2OeLuer77":
+        Script8();
+        break;
+      case "69hc0V22i7o":
+        Script9();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script8()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script9()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
