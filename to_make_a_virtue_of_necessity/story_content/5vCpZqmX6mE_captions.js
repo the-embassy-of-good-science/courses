bd5c08133c81a06b21e43f2aa3c27ca1e871c79f
armsch <81294702+armsch@users.noreply.github.com>
2021-06-24 16:02:56 +0200

    Delete to_make_a_virtue_of_necessity directory

--- to_make_a_virtue_of_necessity/story_content/5vCpZqmX6mE_captions.js
+++
@@ -1,1 +0,0 @@
-window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%206.43.22859.0%0D%0A%0D%0A00:00:00.090%20--%3E%2000:00:04.364%0D%0AAs%20researchers,%20we%20are%20expected%20to%20commit%20to%20the%20struggle%20of%20pursuing%20the%20truth.%0D%0A%0D%0A"}')
