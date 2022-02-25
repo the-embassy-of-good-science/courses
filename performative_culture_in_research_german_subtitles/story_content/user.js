54f37166b8f4816dfcebc8cacf56f705d1c6dd29
armsch <81294702+armsch@users.noreply.github.com>
2022-02-25 14:07:38 +0100

    Add files via upload

---
+++ performative_culture_in_research_german_subtitles/story_content/user.js
@@ -0,0 +1,91 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6QHg3A82qqj":
+        Script1();
+        break;
+      case "6CQSFrsD1aO":
+        Script2();
+        break;
+      case "6426G1svacB":
+        Script3();
+        break;
+      case "6RM5SAMl75W":
+        Script4();
+        break;
+      case "6pTOHAVyDud":
+        Script5();
+        break;
+      case "6JKFAbAJJWh":
+        Script6();
+        break;
+      case "6S0fzHg24QW":
+        Script7();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
