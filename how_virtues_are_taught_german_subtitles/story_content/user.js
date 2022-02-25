405a30b742b7b17c3b29121892425e56848fb58a
armsch <81294702+armsch@users.noreply.github.com>
2022-02-25 13:56:30 +0100

    Add files via upload

---
+++ how_virtues_are_taught_german_subtitles/story_content/user.js
@@ -0,0 +1,91 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "64FGDr8b0AA":
+        Script1();
+        break;
+      case "5lny6jUrVSm":
+        Script2();
+        break;
+      case "5blDQLfUHSA":
+        Script3();
+        break;
+      case "6K3i28skSV6":
+        Script4();
+        break;
+      case "62CZAWmlQ9D":
+        Script5();
+        break;
+      case "5oZBsLv7YQe":
+        Script6();
+        break;
+      case "6QwHRgokd1u":
+        Script7();
+        break;
+  }
+}
+
+function Script1()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script3()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script4()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script5()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script6()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
+function Script7()
+{
+  var player = GetPlayer();
+
+var numValue = player.GetVar("Pagecountpercentage");
+var JSRoundedNum = numValue.toFixed(0);
+player.SetVar("Pagecountpercentage",JSRoundedNum);
+}
+
